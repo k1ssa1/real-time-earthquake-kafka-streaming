4425d0b4f712a1ff5671ec8eab3c91b42d2abc34
k1ssa1 <samikissai.edu@gmail.com>
2026-09-13 18:28:47 +0000

    feat: add message

--- src/main/java/EarthquakeProducer.java
+++ src/main/java/EarthquakeProducer.java
@@ -1,4 +1,26 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class EarthquakeProducer {
-
+	String bootstrapServers = "localhost:9092";
+	final String topic ="earthquakes";
+	
+	Properties properties = new Properties() {{
+		put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		put(ProducerConfig.ACKS_CONFIG, "1");
+	}};
+	
+	Producer<String, String> producer = new KafkaProducer<>(properties);
+	
+	public void send(String key, String value) {
+		ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+		producer.send(record);
+	}
 }
